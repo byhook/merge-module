504faa4f31e33f6e4117d265932c243b8d31a0fe
Andy <byhook@163.com>
2018-06-19 12:03:02 +0800

    合并组件

--- app/src/main/java/com/onzhou/module/MainActivity.java
+++ app/src/main/java/com/onzhou/module/MainActivity.java
@@ -1,7 +1,13 @@
 package com.onzhou.module;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.onzhou.module.download.DownloadActivity;
+import com.onzhou.module.liveroom.LiveRoomActivity;
+import com.onzhou.module.upload.UploadActivity;
+import com.onzhou.module.video.VideoPlayActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onDownloadClick(View view) {
+        DownloadActivity.intentStart(this);
+    }
+
+    public void onUploadClick(View view) {
+        UploadActivity.intentStart(this);
+    }
+
+
+    public void onLiveClick(View view) {
+        LiveRoomActivity.intentStart(this);
+    }
+
+    public void onVideoClick(View view) {
+        VideoPlayActivity.intentStart(this);
     }
 }
